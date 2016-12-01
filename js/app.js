e97245eab2af3013eed920c5c2190042a0f6770e
Matias <matisyo@users.noreply.github.com>
2016-12-01 15:31:11 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -15,45 +15,7 @@ var app = angular.module('jobifyApp', [ 'ui.bootstrap', 'angular.filter',
 
 app
 		.config(function($routeProvider, $locationProvider, $httpProvider) {
-			
-	
-		    var $http,
-			interceptor = ['$q', '$injector', function ($q, $injector) {
-
-			    function success(response) {
-				return response;
-			    }
-
-			    function error(response) {
-				if (response.status === 404 && response.config.url.indexOf(".html")) {
-
-				    // get $http via $injector because of circular dependency problem
-				    $http = $http || $injector.get('$http');
-
-				    var defer = $q.defer();
-				    $http.get('404.html')
-					.then(function (result) {
-					    response.status = 200;
-					    response.data = result.data;
-					    defer.resolve(response);
-					}, function () {
-					    defer.reject(response);
-					});
-
-				    return defer.promise;// response;
-				} else {
-				    return $q.reject(response);
-				}
-			    }
-
-			    return function (promise) {
-				return promise.then(success, error);
-			    }
-			}];
-
-		    $httpProvider.responseInterceptors.push(interceptor);
-	
-	
+		
 			$routeProvider.when('/login', {
 				templateUrl : 'partials/login.html',
 				controller : 'LoginController'
